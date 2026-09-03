fdfd9aed0a4b3e5d8a41f49604521ec2cf5e7c46
fsmkh1-crypto <fsmkh1@gmail.com>
2026-09-03 09:57:04 +0900

    Update asset-role tests for approved desktop pack

--- tests/test_asset_manager.py
+++ tests/test_asset_manager.py
@@ -8,23 +8,26 @@ from asset_manager import resolve_asset, role_for_dialogue, role_for_work_mode
 class AssetManagerTests(unittest.TestCase):
     def test_work_modes_map_to_expected_character_roles(self):
         self.assertEqual(role_for_work_mode("normal"), "default")
-        self.assertEqual(role_for_work_mode("wind_down"), "worry")
+        self.assertEqual(role_for_work_mode("wind_down"), "rest")
         self.assertEqual(role_for_work_mode("leave"), "praise")
         self.assertEqual(role_for_work_mode("late_leave"), "nag")
         self.assertEqual(role_for_work_mode("hard_stop"), "nag")
 
     def test_dialogue_role_respects_leave_mode(self):
         self.assertEqual(role_for_dialogue("cheer", "normal"), "cheer")
+        self.assertEqual(role_for_dialogue("away_start", "normal"), "away")
+        self.assertEqual(role_for_dialogue("break", "normal"), "rest")
+        self.assertEqual(role_for_dialogue("stats", "normal"), "stats")
         self.assertEqual(role_for_dialogue("cheer", "leave"), "praise")
         self.assertEqual(role_for_dialogue("playful", "late_leave"), "nag")
 
-    def test_resolver_prefers_png_but_accepts_jpg(self):
+    def test_resolver_prefers_canonical_png_but_accepts_legacy(self):
         with tempfile.TemporaryDirectory() as td:
             root = Path(td)
             (root / "nag.jpg").write_bytes(b"x")
             self.assertEqual(resolve_asset("nag", root), root / "nag.jpg")
-            (root / "nag.png").write_bytes(b"x")
-            self.assertEqual(resolve_asset("nag", root), root / "nag.png")
+            (root / "warning_kyunghee.png").write_bytes(b"x")
+            self.assertEqual(resolve_asset("nag", root), root / "warning_kyunghee.png")
 
     def test_missing_asset_is_safe(self):
         with tempfile.TemporaryDirectory() as td:
